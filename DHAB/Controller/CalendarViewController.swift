19e391b27ae3d138d3848d88e43880507b858ced
Yuta Seto <setoon47@yahoo.co.jp>
2023-01-13 17:09:04 +0900

    家計簿画面のセグメンテッドコントロールの追加

--- DHAB/Controller/CalendarViewController.swift
+++ DHAB/Controller/CalendarViewController.swift
@@ -10,6 +10,17 @@ import FSCalendar
 import RealmSwift
 import UIKit
 
-class CalendarViewController:UIViewController{
+class CalendarViewController:UIViewController,UITableViewDelegate,UITableViewDataSource{
+    
+    @IBOutlet weak var calendarView: FSCalendar!
+    @IBOutlet weak var householdAccountBookTableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
     
 }
--- DHAB/Controller/HouseholdAccountBookViewController.swift
+++ DHAB/Controller/HouseholdAccountBookViewController.swift
@@ -12,4 +12,91 @@ import Charts
 
 class HouseholdAccountBookViewController:UIViewController{
     
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var dayBackButton: UIButton!
+    @IBOutlet weak var dayPassButton: UIButton!
+    @IBOutlet weak var householdAccountBookSegmentedControl: UISegmentedControl!
+    
+    @IBAction func householdAccountBookSegmentedControl(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex{
+        case 0:
+            addPaymentView()
+        case 1:
+            addIncomeView()
+        case 2:
+            addSavingView()
+        default:
+            return
+        }
+    }
+    
+    @IBAction func dayBackButton(_ sender: UIButton) {
+        dayBack()
+    }
+    
+    @IBAction func dayPassButton(_ sender: UIButton) {
+        dayPass()
+    }
+    
+    func dayBack(){
+            date = Calendar.current.date(byAdding: .month, value: -1, to: date)!
+            dayLabel.text = dateFormatter.string(from: date)
+    }
+    
+    func dayPass(){
+            date = Calendar.current.date(byAdding: .month, value: 1, to: date)!
+            dayLabel.text = dateFormatter.string(from: date)
+    }
+    
+    var date = Date()
+    
+    var dateFormatter: DateFormatter{
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yy年MM月"
+        dateFormatter.locale = Locale(identifier: "ja-JP")
+        return dateFormatter
+    }
+    
+    //subView関連
+    @IBOutlet var paymentView: UIView!
+    @IBOutlet var incomeView: UIView!
+    @IBOutlet var savingView: UIView!
+    
+    func addPaymentView(){
+        incomeView.removeFromSuperview()
+        savingView.removeFromSuperview()
+        self.view.addSubview(paymentView)
+    }
+    func addIncomeView(){
+        paymentView.removeFromSuperview()
+        savingView.removeFromSuperview()
+        self.view.addSubview(incomeView)
+    }
+    
+    func addSavingView(){
+        paymentView.removeFromSuperview()
+        incomeView.removeFromSuperview()
+        self.view.addSubview(savingView)
+    }
+    
+    func settingSubView(){
+        paymentView.frame = CGRect(x: 0,
+                                                y: householdAccountBookSegmentedControl.frame.minY + householdAccountBookSegmentedControl.frame.height,
+                                                width: self.view.frame.width,
+                                                height: (self.view.frame.height - householdAccountBookSegmentedControl.frame.minY))
+        incomeView.frame = CGRect(x: 0,
+                                 y: householdAccountBookSegmentedControl.frame.minY + householdAccountBookSegmentedControl.frame.height,
+                                 width: self.view.frame.width,
+                                 height: (self.view.frame.height - householdAccountBookSegmentedControl.frame.minY))
+        savingView.frame = CGRect(x: 0,
+                                 y: householdAccountBookSegmentedControl.frame.minY + householdAccountBookSegmentedControl.frame.height,
+                                 width: self.view.frame.width,
+                                 height: (self.view.frame.height - householdAccountBookSegmentedControl.frame.minY))
+    }
+    
+    override func viewDidLoad() {
+        dayLabel.text = dateFormatter.string(from:date)
+        addPaymentView()
+        settingSubView()
+    }
 }
