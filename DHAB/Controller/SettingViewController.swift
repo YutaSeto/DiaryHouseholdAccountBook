5b5a52e994a51bea5920c11734b982c7ab4337ad
Yuta Seto <setoon47@yahoo.co.jp>
2023-01-13 13:54:43 +0900

    設定画面のTableViewの追加

--- DHAB/Controller/SettingViewController.swift
+++ DHAB/Controller/SettingViewController.swift
@@ -9,6 +9,39 @@ import Foundation
 import UIKit
 import RealmSwift
 
-class SettingViewController:UIViewController{
+class SettingViewController:UIViewController,UITableViewDelegate,UITableViewDataSource{
     
+    @IBOutlet weak var settingTableView: UITableView!
+        
+    var paymentList = [(String)]()
+    var settingList = ["費目の設定","予算の設定","通知の設定"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        settingList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = settingTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel!.text = settingList[indexPath.row]
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        settingTableView.delegate = self
+        settingTableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row{
+        case 0:
+            return
+        case 1:
+            return
+        case 2:
+            return
+        default:
+            return
+        }
+    }
 }
