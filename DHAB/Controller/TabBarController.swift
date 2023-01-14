0956ddffb1394d4a8d265465a31ddf8808384bcd
Yuta Seto <setoon47@yahoo.co.jp>
2023-01-14 21:17:29 +0900

    指摘事項の修正

--- DHAB/Controller/TabBarController.swift
+++ DHAB/Controller/TabBarController.swift
@@ -9,5 +9,18 @@ import Foundation
 import UIKit
 
 class TabBarController:UITabBarController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let names = ["InputViewController","CalendarViewController","HouseholdAccountBookViewController","DiaryViewController","SettingViewController"]
+        var viewControllers = [UIViewController]()
+        for name in names {
+            let storyboard = UIStoryboard(name: name, bundle: nil)
+            if let viewController = storyboard.instantiateInitialViewController(){
+                viewControllers.append(viewController)
+            }
+        }
+        setViewControllers(viewControllers, animated: false)
+        
+    }
     
 }
