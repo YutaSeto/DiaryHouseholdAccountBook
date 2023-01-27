28e75f1ebc7a0896a5fb60541af6f88a76d90f2b
Yuta Seto <setoon47@yahoo.co.jp>
2023-01-27 22:48:26 +0900

    予算の設定で行き詰まる。ブランチを変えて再構築

--- DHAB/Controller/BudgetViewController.swift
+++ DHAB/Controller/BudgetViewController.swift
@@ -74,6 +74,7 @@ class BudgetViewController: UIViewController{
         setPaymentData()
         setExpenceItemData()
         setNavigationBarButton()
+        newTableView()
     }
     
     func setExpenceItemData(){
@@ -87,6 +88,19 @@ class BudgetViewController: UIViewController{
         let realm = try! Realm()
         let result = realm.objects(PaymentBudgetModel.self)
         paymentBudgetList = Array(result)
+    }
+    
+    func newTableView(){
+        let realm = try! Realm()
+        let result = realm.objects(PaymentBudgetModel.self)
+        expenceItemList.forEach{ item in
+            for i in 0 ..< expenceItemList.count{
+                paymentBudgetList[i].budgetExpenceItem = expenceItemList[i].category
+                paymentBudgetList[i].budgetDate = date
+            }
+        }
+        paymentBudgetList = Array(result)
+        
     }
 }
 
@@ -105,28 +119,111 @@ extension BudgetViewController:UITableViewDelegate,UITableViewDataSource{
         for i in 0 ..< paymentBudgetList.count{
             if dateFormatter.string(from: paymentBudgetData[i].budgetDate) == dateFormatter.string(from: date) && paymentBudgetData[i].budgetExpenceItem == cell.budgetExpenceItemLabel.text{
                 cell.budgetPriceLabel.text! = String(paymentBudgetData[i].budgetPrice)
+                print("何を保存しているのか\(cell.budgetPriceLabel.text!)")
                 break
-            } else {
-                cell.budgetPriceLabel.text = "0"
             }
         }
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = budgetTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! BudgetTableViewCell
+        print(cell.budgetPriceLabel.text!)
         let expenceItemCell = expenceItemList[indexPath.row]
-        let budgetCell = paymentBudgetList[indexPath.row]
+        
+        cell.budgetExpenceItemLabel.text = expenceItemList[indexPath.row].category
         budgetViewControllerDelegate = self
         
+        let realm = try! Realm()
+        let paymentBudgetData = realm.objects(PaymentBudgetModel.self)
+        
         let alert = UIAlertController(title:"予算を変更します", message: nil, preferredStyle: .alert)
+        
         var textFieldOnAlert = UITextField()
         alert.addTextField{textField in
             textFieldOnAlert = textField
             textField.placeholder = "0"
         }
         
-        print("タップされたよ\(budgetCell.budgetPrice)")
-        if budgetCell.budgetPrice < 0 {
+        var number = -1
+        func dateAndExpenceItemEqual() -> Int{
+            for i in 0 ..< paymentBudgetList.count{
+                if dateFormatter.string(from: paymentBudgetData[i].budgetDate) == dateFormatter.string(from: date) && paymentBudgetData[i].budgetExpenceItem == cell.budgetExpenceItemLabel.text && paymentBudgetData[i].budgetPrice == Int(cell.budgetPriceLabel.text!){
+                    number = i
+                    break
+                }else{
+                    return -1
+                }
+            }
+            return number
+        }
+        func dateOrExpenceItemNotEqual() -> Int{
+            for i in 0 ..< paymentBudgetList.count{
+                if dateFormatter.string(from: paymentBudgetData[i].budgetDate) != dateFormatter.string(from: date){
+                    if paymentBudgetData[i].budgetExpenceItem != cell.budgetExpenceItemLabel.text{
+                        number = i
+                        break
+                    }else{
+                        
+                        return -1
+                    }
+                }else{
+                    return -1
+                }
+            }
+            return number
+        }
+        
+        if paymentBudgetList == []{
+            let add = UIAlertAction(title:"追加する",style: .default, handler:{(action) ->Void in
+                let budgetData = PaymentBudgetModel()
+                try! realm.write{
+                    budgetData.budgetExpenceItem = expenceItemCell.category
+                    budgetData.budgetPrice = Int(textFieldOnAlert.text!)!
+                    budgetData.budgetDate = self.date
+                    cell.budgetPriceLabel.text = textFieldOnAlert.text!//ここが設定できない
+                    realm.add(budgetData)
+                    print(cell.budgetPriceLabel.text!)
+                    print(cell)
+                }
+                self.budgetViewControllerDelegate?.updateList()
+                self.budgetTableView.reloadData()
+            })
+            
+            let cancel = UIAlertAction(title:"キャンセル", style: .default, handler:{(action) -> Void in
+                return
+            })
+            
+            alert.addAction(add)
+            alert.addAction(cancel)
+            
+            self.present(alert,animated: true, completion: nil)
+        }else if dateAndExpenceItemEqual() >= 0{
+            let edit = UIAlertAction(title:"修正する",style: .default, handler:{(action) ->Void in
+                let realm = try! Realm()
+                let budgetData = realm.objects(PaymentBudgetModel.self).filter("budgetExpenceItem CONTAINS %@" ,expenceItemCell.category)
+                try! realm.write{
+                    budgetData[0].budgetExpenceItem = expenceItemCell.category
+                    budgetData[0].budgetPrice = Int(textFieldOnAlert.text!)!
+                    budgetData[0].budgetDate = self.date
+                }
+                self.budgetViewControllerDelegate?.updateList()
+                self.budgetTableView.reloadData()
+            })
+            
+            let cancel = UIAlertAction(title:"キャンセル", style: .default, handler:{(action) -> Void in
+                return
+            })
+            
+            alert.addAction(edit)
+            alert.addAction(cancel)
+            self.present(alert,animated: true, completion: nil)
+            
+            print(dateAndExpenceItemEqual())
+            print(dateOrExpenceItemNotEqual())
+            
+            
+        }else if dateOrExpenceItemNotEqual() >= 0{
             let add = UIAlertAction(title:"追加する",style: .default, handler:{(action) ->Void in
                 let realm = try! Realm()
                 let budgetData = PaymentBudgetModel()
@@ -138,7 +235,6 @@ extension BudgetViewController:UITableViewDelegate,UITableViewDataSource{
                 }
                 self.budgetViewControllerDelegate?.updateList()
                 self.budgetTableView.reloadData()
-                print("text == 0 \(budgetData)")
             })
             
             let cancel = UIAlertAction(title:"キャンセル", style: .default, handler:{(action) -> Void in
@@ -149,29 +245,9 @@ extension BudgetViewController:UITableViewDelegate,UITableViewDataSource{
             alert.addAction(cancel)
             
             self.present(alert,animated: true, completion: nil)
+            
         }else{
-            let add = UIAlertAction(title:"修正する",style: .default, handler:{(action) ->Void in
-                let realm = try! Realm()
-                let budgetData = realm.objects(PaymentBudgetModel.self).filter("budgetExpenceItem CONTAINS %@" ,expenceItemCell.category)
-                try! realm.write{
-                    budgetData[0].budgetExpenceItem = expenceItemCell.category
-                    budgetData[0].budgetPrice = Int(textFieldOnAlert.text!)!
-                    budgetData[0].budgetDate = self.date
-                }
-                self.budgetViewControllerDelegate?.updateList()
-                print("text != 0 \(budgetData)")
-                
-                self.budgetTableView.reloadData()
-            })
-            
-            let cancel = UIAlertAction(title:"キャンセル", style: .default, handler:{(action) -> Void in
-                return
-            })
-            
-            alert.addAction(add)
-            alert.addAction(cancel)
-            
-            self.present(alert,animated: true, completion: nil)
+            print("その他")
         }
     }
 }
