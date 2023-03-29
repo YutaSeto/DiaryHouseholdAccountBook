e5895618be0d5198c641e7f9bc62f16f0e0f3687
Yuta Seto <setoon47@yahoo.co.jp>
2023-03-29 13:44:08 +0900

    オリジナルセルを透明に変更

--- DHAB/View/customCell/FSCalendarCustomCell.swift
+++ DHAB/View/customCell/FSCalendarCustomCell.swift
@@ -14,10 +14,26 @@ class FSCalendarCustomCell: FSCalendarCell {
     @IBOutlet weak var paymentLabel:UILabel!
     @IBOutlet weak var incomeLabel:UILabel!
     
+    var collectionViewDate:Date?
+    var labelsDate:Date?
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        self.layer.borderWidth = 1.0
+        self.layer.borderColor = UIColor.lightGray.cgColor // 枠線の色
     }
-
+    
+    var date: Date!{
+        didSet{
+            self.labelsDate = date
+        }
+    }
+    
+    func toggleSelection(){
+        if labelsDate?.zeroclock == collectionViewDate?.zeroclock{
+            self.contentView.backgroundColor = .lightGray
+        }
+    }
+    
 }
